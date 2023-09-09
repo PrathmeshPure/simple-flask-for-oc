d7f0930ced9e9a3b779acdcf339f8b8fb526af88
Prathmesh Pure <pureprathmesh@gmail.com>
2023-09-09 23:49:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!from docker'
+    return 'Hello World!'
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug = True)
